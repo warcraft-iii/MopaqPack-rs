de308f11c96b7cf270622118251bc2213cfdacb5
Jai <814683@qq.com>
2019-08-06 23:36:01 +0800

    init repo

---
+++ libs/ceres-mpq/src/lib.rs
@@ -0,0 +1,272 @@
+#![feature(custom_attribute)]
+
+use std::ffi::{c_void, CString};
+use std::ptr;
+
+use num_derive::FromPrimitive;
+use num_traits::FromPrimitive;
+
+use failure::Fail;
+
+use storm_sys as storm;
+
+#[derive(FromPrimitive, Clone, Copy, Debug)]
+pub enum GenericErrorCode {
+    NoError             = 0,
+    FileNotFound        = 2,
+    AccessDenied        = 1,
+    InvalidHandle       = 9,
+    NotEnoughMemory     = 12,
+    NotSupported        = 95,
+    InvalidParameter    = 22,
+    DiskFull            = 28,
+    AlreadyExists       = 17,
+    InsufficientBuffer  = 105,
+    BadFormat           = 1000,
+    NoMoreFiles         = 1001,
+    HandleEof           = 1002,
+    CanNotComplete      = 1003,
+    FileCorrupt         = 1004,
+    AviFile             = 10000,
+    UnknownFileKey      = 10001,
+    ChecksumError       = 10002,
+    InternalFile        = 10003,
+    BaseFileMissing     = 10004,
+    MarkedForDelete     = 10005,
+    FileIncomplete      = 10006,
+    UnknownFileNames    = 10007,
+    CantFindPatchPrefix = 10008,
+}
+
+#[derive(Debug, Fail)]
+pub enum GenericError {
+    #[fail(display = "Success")]
+    Success,
+    #[fail(display = "Error code {:?}", _0)]
+    ErrorCode(GenericErrorCode),
+    #[fail(display = "Unknown error code {:?}", _0)]
+    Unknown(u32),
+}
+
+fn get_last_generic_error() -> GenericError {
+    let error_code_id = unsafe { storm::GetLastError() };
+
+    let error_code: Option<GenericErrorCode> = FromPrimitive::from_u32(error_code_id);
+
+    if let Some(error_code) = error_code {
+        match error_code {
+            GenericErrorCode::NoError => GenericError::Success,
+            error_code => GenericError::ErrorCode(error_code),
+        }
+    } else {
+        GenericError::Unknown(error_code_id)
+    }
+}
+
+fn test_for_generic_error() -> Result<(), GenericError> {
+    let error = get_last_generic_error();
+
+    if let GenericError::Success = error {
+        Ok(())
+    } else {
+        Err(error)
+    }
+}
+
+impl std::string::ToString for GenericErrorCode {
+    fn to_string(&self) -> String {
+        match self {
+            GenericErrorCode::NoError => "No error".into(),
+            GenericErrorCode::FileNotFound => "File not found".into(),
+            GenericErrorCode::AccessDenied => "Access denied".into(),
+            _ => format!("Error Code: {}", *self as u32),
+        }
+    }
+}
+
+#[derive(FromPrimitive)]
+pub enum SignatureErrorKind {
+    NoSignature          = 0,
+    VerifyFailed         = 1,
+    WeakSignatureOk      = 2,
+    WeakSignatureError   = 3,
+    StrongSignatureOk    = 4,
+    StrongSignatureError = 5,
+}
+
+pub struct MPQArchive {
+    handle: storm::HANDLE,
+}
+
+pub struct MPQFile {
+    handle: storm::HANDLE,
+}
+
+impl MPQArchive {
+    pub fn open(path: &str) -> Result<MPQArchive, GenericError> {
+        let path = CString::new("flat-file://".to_string() + path).unwrap();
+        let path_ptr = path.as_ptr();
+        let mut handle = ptr::null_mut();
+
+        unsafe {
+            storm::SFileOpenArchive(path_ptr, 0, 0, &mut handle);
+        }
+
+        test_for_generic_error()?;
+
+        Ok(MPQArchive { handle })
+    }
+
+    pub fn create(path: &str, filecount: usize, use_filelist: bool) -> Result<MPQArchive, GenericError> {
+        let path = CString::new(path).unwrap();
+        let path_ptr = path.as_ptr();
+        let mut handle = ptr::null_mut();
+
+        let mut flags = 0;
+        let dwMpqVersion = (flags & storm::MPQ_CREATE_ARCHIVE_VMASK) >> 24;
+        let dwStreamFlags = 0;
+        let mut dwFileFlags1 = if flags & storm::MPQ_CREATE_LISTFILE != 0 { storm::MPQ_FILE_DEFAULT_INTERNAL } else { 0 };
+        let dwFileFlags2 = if flags & storm::MPQ_CREATE_ATTRIBUTES != 0 { storm::MPQ_FILE_DEFAULT_INTERNAL } else { 0 };
+        let dwFileFlags3 = if flags & storm::MPQ_CREATE_SIGNATURE != 0 { storm::MPQ_FILE_DEFAULT_INTERNAL } else { 0 };
+        let mut dwAttrFlags =
+            if flags & storm::MPQ_CREATE_ATTRIBUTES != 0 { storm::MPQ_ATTRIBUTE_CRC32 | storm::MPQ_ATTRIBUTE_FILETIME | storm::MPQ_ATTRIBUTE_MD5 } else { 0 };
+        let dwSectorSize = if dwMpqVersion >= storm::MPQ_FORMAT_VERSION_3 {  0x4000 } else { 0x1000 };
+        let dwRawChunkSize = if dwMpqVersion >= storm::MPQ_FORMAT_VERSION_4 { 0x4000 } else { 0 };
+        let dwMaxFileCount = filecount;
+
+        if (dwMpqVersion >= storm::MPQ_FORMAT_VERSION_3 && flags & storm::MPQ_CREATE_ATTRIBUTES != 0)
+        {
+            dwAttrFlags |= storm::MPQ_ATTRIBUTE_PATCH_BIT;
+        }
+
+        if (use_filelist)
+        {
+            dwFileFlags1 = storm::MPQ_FILE_DEFAULT_INTERNAL;
+        }
+
+        unsafe {
+            let cbSize = ::std::mem::size_of::<storm::_SFILE_CREATE_MPQ>() as u32;
+
+            let mut ci = Box::new(storm::_SFILE_CREATE_MPQ{
+                cbSize: cbSize,
+                dwMpqVersion: dwMpqVersion,
+                pvUserData: ptr::null_mut(),
+                cbUserData: 0,
+                dwStreamFlags: dwStreamFlags,
+                dwFileFlags1: dwFileFlags1,
+                dwFileFlags2: dwFileFlags2,
+                dwFileFlags3: dwFileFlags3,
+                dwAttrFlags: dwAttrFlags,
+                dwSectorSize: dwSectorSize,
+                dwRawChunkSize: dwRawChunkSize,
+                dwMaxFileCount: dwMaxFileCount as u32,
+            });
+
+            storm::SFileCreateArchive2(path_ptr, &mut *ci, &mut handle);
+        }
+
+        test_for_generic_error()?;
+
+        Ok(MPQArchive { handle })
+    }
+
+    pub fn open_file(&self, file_name: &str) -> Result<MPQFile, GenericError> {
+        let file_name = CString::new(file_name).unwrap();
+        let file_name_ptr = file_name.as_ptr();
+        let mut handle = ptr::null_mut();
+
+        unsafe {
+            storm::SFileOpenFileEx(self.handle, file_name_ptr, 0, &mut handle);
+        }
+
+        test_for_generic_error()?;
+
+        Ok(MPQFile { handle })
+    }
+
+    pub fn write_file(&self, file_name: &str, data: &[u8]) -> Result<(), GenericError> {
+        let file_name = CString::new(file_name).unwrap();
+        let file_name_ptr = file_name.as_ptr();
+        let mut handle = ptr::null_mut();
+
+        unsafe {
+            if !storm::SFileCreateFile(
+                self.handle,
+                file_name_ptr,
+                0,
+                data.len() as u32,
+                0,
+                storm::MPQ_FILE_REPLACEEXISTING,
+                &mut handle,
+            ) {
+                test_for_generic_error()?;
+            }
+        }
+
+        unsafe {
+            if !storm::SFileWriteFile(handle, data.as_ptr() as *const c_void, data.len() as u32, 0)
+            {
+                test_for_generic_error()?;
+            }
+        }
+
+        unsafe {
+            if !storm::SFileFinishFile(handle) {
+                test_for_generic_error()?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Drop for MPQArchive {
+    fn drop(&mut self) {
+        unsafe {
+            storm::SFileCloseArchive(self.handle);
+        }
+    }
+}
+
+impl MPQFile {
+    pub fn get_size(&self) -> Result<u32, GenericError> {
+        let mut file_size_high = 0;
+
+        let file_size_low = unsafe { storm::SFileGetFileSize(self.handle, &mut file_size_high) };
+
+        test_for_generic_error()?;
+
+        Ok(file_size_low)
+    }
+
+    pub fn read_contents(&self) -> Result<Vec<u8>, GenericError> {
+        let size = self.get_size()?;
+        let mut buffer: Vec<u8> = Vec::new();
+        buffer.resize_with(size as usize, || 0);
+
+        let buffer_ptr = buffer.as_mut_ptr() as *mut c_void;
+        let mut bytes_read: u32 = 0;
+
+        unsafe {
+            if !storm::SFileReadFile(
+                self.handle,
+                buffer_ptr,
+                size,
+                &mut bytes_read,
+                ptr::null_mut(),
+            ) {
+                test_for_generic_error()?;
+            }
+        }
+
+        Ok(buffer)
+    }
+}
+
+impl Drop for MPQFile {
+    fn drop(&mut self) {
+        unsafe {
+            storm::SFileCloseFile(self.handle);
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,120 @@
+
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
+use failure::{Error};
+
+use std::collections::HashMap;
+use std::fs;
+
+type FileList = HashMap<String, String>;
+
+fn main() -> Result<(), Error> {
+
+    let matches = App::new("MopaqPack-rs")
+                    .version("1.0")
+                    .author("Jai <814683@qq.com>")
+                    .about("Generate Warcraft III map file")
+                    .arg(Arg::with_name("output")
+                            .short("o")
+                            .long("output")
+                            .value_name("FILE")
+                            .help("Output file name")
+                            .takes_value(true))
+                    .arg(Arg::with_name("filelist")
+                            .short("f")
+                            .long("filelist")
+                            .help("Input file list file"))
+                    .arg(Arg::with_name("input")
+                            .short("i")
+                            .long("input")
+                            .value_name("FILE")
+                            .help("Input directory or file list")
+                            .takes_value(true))
+                    .get_matches();
+
+    std::process::exit(match run(matches){
+        
+        Err(error) => {
+            println!("[ERROR] An error has occured. Error chain:");
+            println!("{}", error);
+
+            for cause in error.iter_causes() {
+                println!("{}", cause);
+            }
+
+            1
+        }
+        Ok(_) => 0,
+
+    });
+
+}
+
+fn run(matches: clap::ArgMatches) -> Result<(), Error> {
+
+    let output = matches.value_of("output").unwrap();
+    let filelist = matches.is_present("filelist");
+    let input = matches.value_of("input").unwrap();
+
+    let files = generate_file_list(input)?;
+
+    exec(&files, output, filelist)?;
+
+    println!("{:?}", files);
+
+    Ok(())
+}
+
+fn generate_file_list(input: &str) -> Result<FileList, Error> {
+
+    let metadata = fs::metadata(input).unwrap();
+
+    let mut files = FileList::new();
+    
+    if metadata.is_dir() {
+        let walker = globwalk::GlobWalkerBuilder::from_patterns(
+            input,
+            &["*.*"],
+        )
+        .build().unwrap()
+        .into_iter()
+        .filter_map(Result::ok);
+        
+        for img in walker {
+            let p = img.path();
+            files.insert(p.strip_prefix(input).unwrap().to_str().unwrap().to_string(), p.to_str().unwrap().to_string());
+        }
+    }
+    else
+    {
+        let json = fs::read_to_string(input)?;
+
+        let data: Vec<Vec<String>> = serde_json::from_str(json.as_str())?;
+
+        for item in data {
+            files.insert(item[0].to_string(), item[1].to_string());
+        }
+
+    }
+
+    Ok(files)
+}
+
+fn exec(files: &FileList, output: &str, filelist: bool) -> Result<bool, Error> {
+
+    if std::path::Path::new(output).exists() {
+        fs::remove_file(output);
+    }
+
+    use ceres_mpq as mpq;
+
+    let archive = mpq::MPQArchive::create(output, files.len(), filelist).unwrap();
+    
+    for (n, p) in files {
+        let data = fs::read(p)?;
+        archive.write_file(n, &*data)?;
+    }
+
+    Ok(true)
+}
